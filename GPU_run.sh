a3406ccd14773e3b671c0ccdfa6a3fc964af298f
Matteo Morellini <matteomorellini08@gmail.com>
2026-05-14 09:12:24 +0200

    fix: force run on Ampere architecture

--- GPU_run.sh
+++ GPU_run.sh
@@ -2,21 +2,15 @@
 #SBATCH --job-name=GPU_deliverable-1
 #SBATCH --output=my_output_%j.out
 #SBATCH --error=my_error_%j.err
-#SBATCH --partition=edu-short
+#SBATCH --partition=edu-medium
 #SBATCH --account=gpu.computing26
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:a30.24:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 module load CUDA/11.8.0
 
-mkdir -p "$HOME/tmp/ncu"
-export TMPDIR="$HOME/tmp/ncu"
-ncu \
-  --kernel-name regex:csr_vector_kernel \
-  --launch-skip 5 \
-  --launch-count 1 \
-  --section SpeedOfLight \
-  --section MemoryWorkloadAnalysis \
-  --section WarpStateStats \
-  ./bin/vector
+./bin/cusparse
+./bin/partial
+./bin/vector
+./bin/adaptive
--- ncu_GPU_run.sh
+++ ncu_GPU_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --partition=edu-short
 #SBATCH --account=gpu.computing26
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:a30.24:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 module load CUDA/11.8.0
@@ -13,10 +13,10 @@ module load CUDA/11.8.0
 mkdir -p "$HOME/tmp/ncu"
 export TMPDIR="$HOME/tmp/ncu"
 ncu \
-  --kernel-name regex:csr_adaptive_kernel \
-  --launch-skip 2 \
+  --kernel-name regex:csr_partial_overlap_kernel \
+  --launch-skip 950 \
   --launch-count 1 \
   --section SpeedOfLight \
   --section MemoryWorkloadAnalysis \
   --section WarpStateStats \
-  ./bin/vector
+  ./bin/partial 
